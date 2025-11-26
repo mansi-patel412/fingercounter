9e89e1f5ee7f01ffd794bf0dc662fdf8325f9d45
mansi-patel412 <mansipatel4124@gmail.com>
2025-11-26 12:35:50 +0530

    modify code

--- fingercounter.py
+++ fingercounter.py
@@ -1,9 +1,9 @@
 import cv2
 import mediapipe as mp
 
-# Create hand detector
+# Create hand detector (tuned confidences for better accuracy)
 mp_hands = mp.solutions.hands
-hands = mp_hands.Hands()
+hands = mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.7)
 mp_draw = mp.solutions.drawing_utils
 
 # Start the camera
@@ -21,7 +21,8 @@ while True:
 
     # If hand detected
     if result.multi_hand_landmarks:
-        for handLms in result.multi_hand_landmarks:
+        # multi_hand_landmarks and multi_handedness are aligned, so use index to get handedness
+        for hand_index, handLms in enumerate(result.multi_hand_landmarks):
             mp_draw.draw_landmarks(img, handLms, mp_hands.HAND_CONNECTIONS)
 
             # Get all landmark positions
@@ -30,17 +31,59 @@ while True:
             for id, lm in enumerate(handLms.landmark):
                 lmList.append((int(lm.x * w), int(lm.y * h)))
 
+            # Make sure we have landmarks
+            if len(lmList) == 0:
+                continue
+
+            # Determine handedness (Left/Right)
+            hand_label = "Unknown"
+            if result.multi_handedness and len(result.multi_handedness) > hand_index:
+                hand_label = result.multi_handedness[hand_index].classification[0].label
+
             # Finger tip landmark IDs
             tipIds = [4, 8, 12, 16, 20]
 
-            # Count fingers
-            if lmList[tipIds[0]][0] > lmList[tipIds[0] - 1][0]:  # Thumb
-                finger_count += 1
-            for id in range(1, 5):
-                if lmList[tipIds[id]][1] < lmList[tipIds[id] - 2][1]:
-                    finger_count += 1
+            # Count fingers for this hand
+            fingers = []
 
-    # Show count on screen
+            # Thumb: use x-coordinate comparison, depends on hand label
+            # For 'Right' hand: thumb is open if tip x > previous joint x
+            # For 'Left' hand: thumb is open if tip x < previous joint x
+            try:
+                if hand_label == "Right":
+                    if lmList[tipIds[0]][0] > lmList[tipIds[0] - 1][0]:
+                        fingers.append(1)
+                    else:
+                        fingers.append(0)
+                else:
+                    if lmList[tipIds[0]][0] < lmList[tipIds[0] - 1][0]:
+                        fingers.append(1)
+                    else:
+                        fingers.append(0)
+            except IndexError:
+                # In case of unexpected landmark indexing issues
+                fingers.append(0)
+
+            # Fingers (index, middle, ring, little): tip y < pip y means finger is up
+            for i in range(1, 5):
+                try:
+                    if lmList[tipIds[i]][1] < lmList[tipIds[i] - 2][1]:
+                        fingers.append(1)
+                    else:
+                        fingers.append(0)
+                except IndexError:
+                    fingers.append(0)
+
+            # Add this hand's fingers to total count
+            finger_count += sum(fingers)
+
+            # (Optional) Show per-hand count near the wrist
+            # wrist coord is landmark 0
+            wrist_x, wrist_y = lmList[0]
+            cv2.putText(img, f"{hand_label}: {sum(fingers)}", (wrist_x - 40, max(wrist_y - 20, 20)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+
+    # Show total count on screen
     cv2.putText(img, f"Fingers: {finger_count}", (30, 80),
                 cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
 
